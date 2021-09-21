053da8d922cf7127e44e85cfa4af10eefa9eb7ec
Babita Thapa <60490773+babitathapa5@users.noreply.github.com>
2021-09-21 15:18:26 -0500

    Adding all the code and required decencies file

---
+++ src/test/java/com/assignment/points/PointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
